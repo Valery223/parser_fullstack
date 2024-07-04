a201af890024bf9e840b5788ac8b5c9e5502ad1f
Valery <teslenko.70@mail.ry>
2024-07-05 01:46:24 +0300

    added visible joined tables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,70 @@
 import React, { useState } from 'react';
-import { Input, Space } from 'antd';
+import { Input, Space, Select, InputNumber } from 'antd';
 import VacancyTable from './componets/VacancyTable';
 import axios from 'axios';
+
+const { Search } = Input;
+const { Option } = Select;
 
 const App = () => {
   const [vacancies, setVacancies] = useState([]);
   const [perPage, setPerPage] = useState(10);
+  const [period, setPeriod] = useState('all'); // '7days', '20days', 'all'
+  const [salary, setSalary] = useState(null);
+  const [currency, setCurrency] = useState('RUR'); // 'RUR', 'EUR'
 
   const fetchVacancy = (value) => {
-    axios.get(`http://127.0.0.1:8000/api/v1/vacancies/?text=${value}`).then(response => {
-      console.log('Response', response.data);
-      setVacancies(response.data.slice(0, perPage));
-    }).catch(error => {
-      console.error('Error fetching vacancies', error);
-    });
+    const params = {
+      text: value,
+      period: period !== 'all' ? period : undefined,
+      salary_from: salary !== null ? salary : undefined,
+      currency: currency
+    };
+
+    axios.get('http://127.0.0.1:8000/api/v1/vacancies/', { params })
+      .then(response => {
+        console.log('Response', response.data);
+        setVacancies(response.data.slice(0, perPage));
+      })
+      .catch(error => {
+        console.error('Error fetching vacancies', error);
+      });
   }
 
-  const { Search } = Input;
   const onSearch = (value, _e, info) => {
     console.log(info?.source, value);
     fetchVacancy(value);
   }
 
   return (
-    <div className="flex">
+    <div >
       <Space direction="vertical">
-        <Search placeholder="input search vacancy" onSearch={onSearch} enterButton />
+        <Search placeholder="input search vacancy" onSearch={onSearch} enterButton style={{ width: 320 }}/>
+        
+        <Select value={period} onChange={setPeriod} style={{ width: 120 }}>
+          <Option value="7">7 дней</Option>
+          <Option value="20">20 дней</Option>
+          <Option value="100">Всё время</Option>
+        </Select>
+
+        <InputNumber 
+          value={salary} 
+          onChange={setSalary} 
+          placeholder="Зарплата" 
+          style={{ width: 200 }} 
+        />
+
+        <Select value={currency} onChange={setCurrency} style={{ width: 120 }}>
+          <Option value="RUR">Руб</Option>
+          <Option value="EUR">Евро</Option>
+        </Select>
+
         <div className="mx-auto my-auto">
+          <Space size={[10, 20]} direction="vertical">
           {vacancies.map((vacancy, index) => (
             <VacancyTable key={index} vacancy={vacancy} />
           ))}
+          </Space>
         </div>
       </Space>
     </div>
--- frontend/src/componets/VacancyTable.jsx
+++ frontend/src/componets/VacancyTable.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
 import { Card } from 'antd';
 
-function VacancyTable({ vacancy }) {
+function VacancyTable({ vacancy, titlePrefix = '' }) {
   const properties = Object.keys(vacancy); // Получаем все ключи объекта vacancy
 
   return (
     <Card
-      title={`Vacancy ${vacancy.id}`} // Используем id в заголовке карточки
+      title={`${titlePrefix}Vacancy ${vacancy.id}`} // Используем id в заголовке карточки
       extra={<a href="#">More</a>}
       style={{
         marginBottom: 16,
         borderRadius: 10, // Пример закругления краев
       }}
-      styles={{
-        header: { // Стили для верхней части (заголовка)
-          backgroundColor: '#1890ff', // Цвет фона заголовка
-          color: '#fff', // Цвет текста заголовка
-          borderTopLeftRadius: 10, // Пример закругления верхнего левого угла
-          borderTopRightRadius: 10, // Пример закругления верхнего правого угла
-        },
-        body: { // Стили для нижней части (содержимого)
-          backgroundColor: '#f0f0f0', // Цвет фона содержимого
-          borderBottomLeftRadius: 10, // Пример закругления нижнего левого угла
-          borderBottomRightRadius: 10, // Пример закругления нижнего правого угла
-        },
+      bodyStyle={{
+        backgroundColor: '#f0f0f0', // Цвет фона содержимого
+        borderBottomLeftRadius: 10, // Пример закругления нижнего левого угла
+        borderBottomRightRadius: 10, // Пример закругления нижнего правого угла
+      }}
+      headStyle={{
+        backgroundColor: '#1890ff', // Цвет фона заголовка
+        color: '#fff', // Цвет текста заголовка
+        borderTopLeftRadius: 10, // Пример закругления верхнего левого угла
+        borderTopRightRadius: 10, // Пример закругления верхнего правого угла
       }}
     >
       {properties.map((key, index) => (
-        vacancy[key] && ( // Проверяем, что значение не пустое
-          <p key={index}><strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong> {vacancy[key]}</p>
+        typeof vacancy[key] === 'object' && vacancy[key] !== null ? (
+          // Если значение является объектом, рекурсивно вызываем VacancyTable
+          <VacancyTable key={index} vacancy={vacancy[key]} titlePrefix={`${key.charAt(0).toUpperCase() + key.slice(1)} `} />
+        ) : (
+          // Иначе просто отображаем значение
+          vacancy[key] && ( // Проверяем, что значение не пустое
+            <p key={index}>
+              <strong>{key.charAt(0).toUpperCase() + key.slice(1)}:</strong> {vacancy[key]}
+            </p>
+          )
         )
       ))}
     </Card>
